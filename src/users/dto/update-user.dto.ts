639b9367eb4e7509520e711c7e260d31a938dc04
Hector Gamboa <hector.gamboarejon.06031.15@gmail.com>
2026-04-21 10:37:31 -0600

    Correccónes al actualizar contraseña de usuario

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,10 +1,8 @@
-import { PartialType, OmitType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 import { IsBoolean, IsOptional } from 'class-validator';
 
-class UpdateUserPayload extends OmitType(CreateUserDto, ['password'] as const) { }
-
-export class UpdateUserDto extends PartialType(UpdateUserPayload) {
+export class UpdateUserDto extends PartialType(CreateUserDto) {
 
   @IsOptional()
   @IsBoolean()
